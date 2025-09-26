fefe0717e2006e9a4b3fd743672285e878967b17
Binenkq <lydiapan150@gmail.com>
2025-09-26 01:31:00 +0100

    Create cancel.js

---
+++ pages/cancel.js
@@ -0,0 +1,8 @@
+export default function Cancel() {
+  return (
+    <main style={{ maxWidth: 700, margin: "40px auto", fontFamily: "sans-serif" }}>
+      <h1>Paiement annulé ❌</h1>
+      <p>Votre paiement a été annulé. Vous pouvez réessayer à tout moment.</p>
+    </main>
+  );
+}
