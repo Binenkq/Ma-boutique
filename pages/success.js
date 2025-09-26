9af0a3cc2829e38b9a143730223f52eb0058afab
Binenkq <lydiapan150@gmail.com>
2025-09-26 01:30:17 +0100

    Create success.js

---
+++ pages/success.js
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <main style={{ maxWidth: 700, margin: "40px auto", fontFamily: "sans-serif" }}>
+      <h1>Paiement réussi ✅</h1>
+      <p>Merci pour votre commande ! Un email de confirmation vous sera envoyé.</p>
+    </main>
+  );
+}
